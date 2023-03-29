4d0a16ad4b35c6693eadab031a3bc82800f79ba5
FranMTrindade <progfrancisco@gmail.com>
2023-03-29 19:48:12 -0300

    primeiro dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,27 +1,76 @@
-import { useContext } from 'react';
-import { AuthContext } from '@/src/contexts/AuthContext';
+import { useEffect, useState } from 'react';
+import { api } from '@/src/services/api';
 import { Header } from '@/src/components/Header';
-import Head from "next/head"
+import axios from 'axios';
+import Head from "next/head";
+import styles from './styles.module.scss'
+
+interface Order {
+  id: number;
+  PC17Pedido: string;
+  PC17DTEmi: string;
+  data: string;
+  numero: string;
+}
 
 export default function Dashboard() {
-  const { user, isAuthenticated } = useContext(AuthContext);
+  const [orders, setOrders] = useState<Order[]>([]);
 
-  if (!isAuthenticated) {
-    // redirect to login page or show an error message
-    return <div>You need to be logged in to access this page.</div>;
-  }
-
-  // return the dashboard component
+  useEffect(() => {
+    async function fetchOrders() {
+      const response = await api.get('/orders')
+      const ordersArray = response.data.Orders as Order[];
+      setOrders(ordersArray);
+    }
+  
+    fetchOrders();
+  }, []);
+  
+  const ordersByYearAndMonth = orders.reduce((acc, order) => {
+    const orderDate = new Date(order.PC17DTEmi);
+    const year = orderDate.getFullYear();
+    const month = orderDate.getMonth();
+    
+    if (!acc[year]) {
+      acc[year] = {};
+    }
+    
+    if (!acc[year][month]) {
+      acc[year][month] = [];
+    }
+    
+    acc[year][month].push(order);
+    
+    return acc;
+  }, {} as { [year: number]: { [month: number]: Order[] } });
+  
   return (
     <>
-    
-    <Head>
-      <title>Elanto - Dashboard</title>
-    </Head>
-    
-      <Header/>
+      <Header />
       <div>
-        <h1>Dashboard!</h1>
+        {Object.entries(ordersByYearAndMonth).map(([year, ordersByMonth]) => (
+          <div key={year}>
+            <h2>{year}</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Mês</th>
+                  <th>Número de Pedidos</th>
+                  <th>Datas dos Pedidos</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.entries(ordersByMonth).map(([month, orders]) => (
+                  <tr key={`${year}-${month}`}>
+                    <td>{new Date(parseInt(year), parseInt(month)).toLocaleString('default', { month: 'long' })}</td>
+                    <td>{orders.length}</td>
+                    <td>{orders.map(order => new Date(order.PC17DTEmi).toLocaleDateString()).join(', ')}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
       </div>
     </>
   );
